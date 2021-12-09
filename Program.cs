1d8533a0b1acbb18561ec8abb26c6faaa744997c
Furkan Cengiz <muhammedcengiz1@gmail.com>
2021-12-09 17:39:16 +0300

    struct yapılar için yeni klasör oluşturuldu ve konuya çalışıldı

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Struct_Yapılar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diktörtgen class_diktörtgen=new Diktörtgen();
+
+        }
+    }
+
+
+    class Diktörtgen{
+
+        public int Kısa_Kenar;
+        public int Uzun_Kenar;
+        public int AlanHesapla(){
+
+            return Kısa_Kenar*Uzun_Kenar;
+        }
+        public Diktörtgen()
+        {
+            Kısa_Kenar = 4;
+            Uzun_Kenar = 3;
+        }
+
+    }
+
+    struct Diktörtgen_Struct{
+
+        public int Kısa_Kenar;
+        public int Uzun_Kenar;
+        public int AlanHesapla(){
+
+            return Kısa_Kenar*Uzun_Kenar;
+        }
+        //public Diktörtgen()
+        //{                         Struct Yapılarda classlardaki gibi parametresiz kurucu metotlar kuramazsın.
+        //    Kısa_Kenar = 4;
+        //    Uzun_Kenar = 3;
+        //}
+
+
+    }
+}
---
+++ Struct Yapılar.md
@@ -0,0 +1,4 @@
+# Struct Yapılar
+Sınıflar gibi tanımlanabilir, yapıcı fonksiyonları, propertyleri, kendi metotları vardır.
+Temeldeki en büyük fark değer tipinde veri olup ram'in stack kısmında tutulması. Hatırla, class, string gibi veri tipleri heap'te tutulurken ref'leri stack bölgesinde olur.
+ 
