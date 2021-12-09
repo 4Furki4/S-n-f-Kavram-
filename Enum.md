a5933f44a97e08f52f51c5d31077042d7440bdf4
Furkan Cengiz <muhammedcengiz1@gmail.com>
2021-12-09 18:14:02 +0300

    Enum öğrenildi ve pratik yapıldı.

---
+++ Enum.md
@@ -0,0 +1,5 @@
+# Enum
+*Enum* anahtar kelimesi _enumeration_ yani numaralandırma kelimesinden gelir. Sayısal verileri string ifadelerle saklamanızı sağlar. Okunabilirliğe katkısı da tam olarak burdan gelir diyebiliriz.
+Sabit değişkenleri saklarken çok kullanılan bir yapıdır, iyi öğrenmeliyim.
+----
+HavaDurumu adlı bir enum kurarsam HavaDurumu.sıcak ile enum değişkene erişirsin ve sıcak diye enum bir değer getirir ancak (int)HavaDurumu.sıcak ile bu stringin tuttuğu sabit değişkeni getiririz.
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ENUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sıcaklık=24;
+
+            if (sıcaklık>=(int)HavaDurumu.CokSıcak)
+            {
+                Console.WriteLine("Bugün dışarı çıkma yoksa çok terlersin!");
+            }
+            else if (sıcaklık>=(int)HavaDurumu.AşırıSıcak)
+            {
+                Console.WriteLine("Kendini buzdolabına sokmalısın!");
+            }
+            else if (sıcaklık<(int)HavaDurumu.Sıcak && sıcaklık>=(int)HavaDurumu.Ilık)
+            {
+                Console.WriteLine("Bugün kendimi sokaklara atacağım!!!!");
+            }
+        }
+    }
+    enum HavaDurumu 
+    {
+        CokSoğuk=0,
+        Soğuk=10,
+        Ilık=20,
+        Sıcak=25,
+        CokSıcak=30,
+        AşırıSıcak=40
+
+
+    }
+}
